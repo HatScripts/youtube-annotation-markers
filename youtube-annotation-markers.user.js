f9a2c3a716947ee7c882f1f0a39718f0ccf9c20e
HatScripts <contact@hatscripts.com>
2017-10-28 16:28:22 +1100

    Convert script to ES6

--- youtube-annotation-markers.user.js
+++ youtube-annotation-markers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        YouTube Annotation Markers
 // @namespace   https://github.com/HatScripts/YouTubeAnnotationMarkers
-// @version     1.2.3
+// @version     1.2.4
 // @description Marks where annotations are on the progress bar of the HTML5 YouTube player.
 // @author      HatScripts
 // @match       http://*.youtube.com/*
@@ -10,21 +10,21 @@
 
 (function () {
     function parseTime(timeStr) {
-        var parts = timeStr.split(":");
-        var duration = 0;
-        for (var multiplier = 1; parts.length > 0; multiplier *= 60) {
+        let parts = timeStr.split(":");
+        let duration = 0;
+        for (let multiplier = 1; parts.length > 0; multiplier *= 60) {
             duration += multiplier * parseFloat(parts.pop());
         }
         return duration;
     }
 
     function getVideoAnnotationXml(videoId, success) {
-        var request = new XMLHttpRequest();
+        let request = new XMLHttpRequest();
         request.open("GET", "annotations_invideo?video_id=" + videoId, true);
-        request.onload = function () {
+        request.onload = () => {
             if (request.status >= 200 && request.status < 400) {
-                var xmlString = request.responseText;
-                var domParser = new DOMParser();
+                let xmlString = request.responseText;
+                let domParser = new DOMParser();
                 success(domParser.parseFromString(xmlString, "text/xml"));
             }
         };
@@ -32,18 +32,19 @@
     }
 
     function annotationToMarker(annotation) {
-        var rectRegions = Array.from(annotation.querySelectorAll("movingRegion *:not([t=never])"));
+        const rectRegions = Array.from(
+            annotation.querySelectorAll("movingRegion *:not([t=never])"));
         if (rectRegions.length === 0) {
             return false;
         }
-        var start = rectRegions[0].getAttribute("t");
-        var end = rectRegions[rectRegions.length - 1].getAttribute("t");
-        var startTime = parseTime(start);
-        var duration = parseTime(end) - startTime;
-        var appearance = annotation.querySelector("appearance");
-        var c = parseInt(appearance.getAttribute("bgColor"), 10);
-        var rgb = [(c & 0xff0000) >> 16, (c & 0x00ff00) >> 8, (c & 0x0000ff)];
-        var marker = document.createElement("div");
+        const start = rectRegions[0].getAttribute("t");
+        const end = rectRegions[rectRegions.length - 1].getAttribute("t");
+        const startTime = parseTime(start);
+        const duration = parseTime(end) - startTime;
+        const appearance = annotation.querySelector("appearance");
+        const c = parseInt(appearance.getAttribute("bgColor"), 10);
+        const rgb = [(c & 0xff0000) >> 16, (c & 0x00ff00) >> 8, (c & 0x0000ff)];
+        const marker = document.createElement("div");
         marker.classList.add("ytp-play-progress");
         marker.style.position = "absolute";
         marker.style.left = ((startTime / videoDuration) * 100) + "%";
@@ -56,27 +57,23 @@
     }
 
     function parseVideoAnnotationXml(xml) {
-        Array.from(xml.querySelectorAll("annotation")).map(function (annotation) {
-            return annotationToMarker(annotation);
-        }).filter(function (annotation) {
-            return annotation;
-        }).sort(function (a1, a2) {
-            return parseInt(a1.style.zIndex) - parseInt(a2.style.zIndex);
-        }).forEach(function (annotation) {
-            progressList.appendChild(annotation);
-        });
+        Array.from(xml.querySelectorAll("annotation"))
+            .map(annotation => annotationToMarker(annotation))
+            .filter(annotation => annotation)
+            .sort((a1, a2) => parseInt(a1.style.zIndex) - parseInt(a2.style.zIndex))
+            .forEach(annotation => progressList.appendChild(annotation));
     }
 
     function getVideoId() {
-        var url = new URL(location.href);
+        const url = new URL(location.href);
         return url.searchParams.get("v") || url.pathname.split("/").pop()
             || document.querySelector("#page-manage ytd-watch").getAttribute("video-id");
     }
 
-    var videoId = getVideoId();
-    var player = document.querySelector("#player");
-    var progressBar = player.querySelector(".ytp-progress-bar");
-    var progressList = progressBar.querySelector(".ytp-progress-list");
-    var videoDuration = parseTime(player.querySelector(".ytp-time-duration").innerText);
+    const videoId = getVideoId();
+    const player = document.querySelector("#player");
+    const progressBar = player.querySelector(".ytp-progress-bar");
+    const progressList = progressBar.querySelector(".ytp-progress-list");
+    const videoDuration = parseTime(player.querySelector(".ytp-time-duration").innerText);
     getVideoAnnotationXml(videoId, parseVideoAnnotationXml);
 })();
